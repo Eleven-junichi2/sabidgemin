a0e11b821a7e6a73db1259d58ac002bb395007c3
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2020-06-24 22:05:03 +0900

    add function to load config

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,6 @@ use std::io::BufReader;
 use std::io::{self, Write};
 use std::path;
 
-struct Config<'a> {
-    language: &'a str,
-    where_to_generate_addon: &'a str,
-}
-
 struct AddonTemplate<'a> {
     addon_name: &'a str,
     author_name: &'a str,
@@ -25,22 +20,34 @@ fn behavior_pack_manifest() {}
 
 fn resource_pack_manifest() {}
 
-fn load_translation(language: &str, file_dir: &path::Path) -> HashMap<String, String> {
-    let translation_file = file_dir.join(format!("{}.json", language));
-    println!("debug: {:?}", &translation_file);
-    let config_file = File::open(translation_file).unwrap();
+fn load_config(file_dir: &path::Path) -> HashMap<String, String> {
+    let file_path = file_dir.join("config.json");
+    let config_file = File::open(file_path).unwrap();
     let reader = BufReader::new(config_file);
+    let config: HashMap<String, String> = serde_json::from_reader(reader).unwrap();
+    config
+}
+
+fn load_translation(language: &str, file_dir: &path::Path) -> HashMap<String, String> {
+    let file_path = file_dir.join(format!("{}.json", language));
+    let translation_file = File::open(file_path).unwrap();
+    let reader = BufReader::new(translation_file);
     let tl: HashMap<String, String> = serde_json::from_reader(reader).unwrap();
-    return tl;
+    tl
 }
 
 fn navigate() {
+    let config = load_config(
+        &path::Path::new(&env::current_exe().unwrap())
+            .parent()
+            .unwrap(),
+    );
     println!("cur_exe: {:?}", env::current_exe().unwrap());
     let translation_file_dir = path::Path::new(&env::current_exe().unwrap())
         .parent()
         .unwrap()
         .join("translation");
-    let tl = load_translation("jp", &translation_file_dir);
+    let tl = load_translation(&config["language"], &translation_file_dir);
     println!("{}", tl["title"]);
     println!("{}", tl["credit"]);
 
